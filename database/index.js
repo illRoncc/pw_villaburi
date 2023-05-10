f9cffd7ccce7efcde955612b0865d49e0db1ec89
illRoncc <roncari13@gmail.com>
2023-05-10 21:53:36 +0200

    bo

--- database/index.js
+++ database/index.js
@@ -10,15 +10,15 @@ app.use(router);
 app.use(bodyParser.json());
 
 router.get("/", function (req, res) {
-    res.sendFile('C:/Users/ronca/Desktop/pw_villaburi/mappa_interattiva/mappa.html')
+    res.sendFile('C:/Users/ronca/Desktop/pw_villaburi/mappa_interattiva/mappa_open_street_map.html')
 });
 
 router.get("/lm", function (req, res) {
-   res.sendFile('D:/ITS/ITS/PWconNodejs/MappaLightMode.html')
+   res.sendFile('C:/Users/ronca/Desktop/pw_villaburi/mappa_interattiva/mappa_light_mode.html')
 });
 
 router.get("/dm", function (req, res) {
-    res.sendFile('D:/ITS/ITS/PWconNodejs/MappaDarkMode.html')
+    res.sendFile('C:/Users/ronca/Desktop/pw_villaburi/mappa_interattiva/mappa_dark_mode.html')
 });
 
 router.get("/mm", function (req, res) {
